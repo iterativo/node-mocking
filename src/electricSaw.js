b9dad6c2edf7a187468fefa7bbc574f6bc4d5cfb
Rafael <torresrafael@gmail.com>
2015-03-20 14:09:52 -0500

    Implemented exception case for electricSaw

--- src/electricSaw.js
+++ src/electricSaw.js
@@ -1,6 +1,10 @@
-function cut(wood){
-	// doing the cutting on wood
-	wood.wasCut = true;
+function cut(errorCallback, wood){
+	if (!wood){
+		errorCallback("Kaboom");
+	} else {
+		// doing the cutting on wood
+		wood.wasCut = true;
+	}
 	return wood;
 }
 
--- test/electricSaw.test.js
+++ test/electricSaw.test.js
@@ -1,11 +1,26 @@
-var should = require("should");
+/*jshint -W030 */
+
+var should = require("should"),
+	sinon = require("sinon");
 
 describe("The electric saw", function() {
-	var electricSaw = require("./../src/electricSaw");
+	var electricSaw = require("./../src/electricSaw"),
+		errorCallback;
+
+	before(function() {
+		errorCallback = sinon.spy();
+	});
 
 	it("should cut wood", function() {
 		var wood = {};
-		electricSaw.cut(wood);
+		electricSaw.cut(errorCallback, wood);
+		errorCallback.called.should.be.false;
 		wood.wasCut.should.be.true;
+	});
+
+	it("should error out when unable to cut wood", function(){
+		var wood = void 0;
+		electricSaw.cut(errorCallback, wood);
+		errorCallback.calledWith("Kaboom").should.be.true;
 	});
 });
